88a15d6a76557005397bcde6c3f3ab3ec2449bc7
mitchiet <mitchiet888@gmail.com>
2021-12-22 17:46:17 -0600

    Added files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starter Template for C++ Projects
+
+This is a starter template for C++ projects using Visual Studio Code.
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string hello_world = "Hello World!";
+    std::cout << hello_world << "\n";
+}
